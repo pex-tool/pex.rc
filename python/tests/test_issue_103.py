4d4077509cbfcf2030b7e1589aa1efd29ff5e959
John Sirois <john.sirois@gmail.com>
2026-05-22 10:05:36 -0700

    Turn off faster test for test_issue_103.

--- python/tests/test_issue_103.py
+++ python/tests/test_issue_103.py
@@ -89,7 +89,5 @@ def test_issue_103(
         args=["-c", "import sqlalchemy"],
         env=dict(PEXRC_ROOT=pexrc_root),
         only_inject=only_inject,
-        # N.B.: Mac SIP causes this 1st run of the --sh-boot variants to be unavoidably slow
-        # when they select an interpreter that is not the current one.
-        assert_faster=not IS_MAC,
+        assert_faster=False,
     )
